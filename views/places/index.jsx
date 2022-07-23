84227af0cda72245de5098ff228213632a63d25b
Charlotte Strickland <charlottemarie81@gmail.com>
2022-07-23 07:49:58 -0400

    Show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -5,10 +5,14 @@ function index(data) {
     let placesFormatted = data.places.map((place) => {
         return (
             <div className="col-sm-6">
-                <h2>{place.name}</h2>
+                <h2>
+                    <a href={`/places/${index}`} >
+                        {place.name}
+                    </a>
+                </h2>
                 <p className="text-center">
                     {place.cuisines}
-                    </p>
+                </p>
                 <img src={place.pic} alt={place.name} />
                 <p className="text-center">
                     Located in {place.city}, {place.state}
